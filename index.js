0e6b68c101032105039925fea613e05cff6478b6
raulrobinson <rasysbox@hotmail.com>
2022-07-21 13:54:51 -0500

    second commit

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const { response } = require('express');
 const express = require('express');
 //const { Pool } = require('pg');
 const app = express();
+//const cors = require('cors');
 const port = process.env.PORT_SRV || 3001;
 
 const merchant_model = require('./merchant_model');
 
 app.use(express.json());
+//app.use(cors());
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
